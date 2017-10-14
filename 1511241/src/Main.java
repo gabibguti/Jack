ac362513d37af2bff28eae04bf3f5dbc5ce5c65c
gabibguti <gabi_guti_1@hotmail.com>
2017-10-14 03:17:02 -0300

    Trying to add array to parent

--- 1511241/src/Main.java
+++ 1511241/src/Main.java
@@ -7,12 +7,19 @@ public class Main {
 		JFrame parent = new JFrame();
 		JButton button = new JButton();
 		JLabel label = new JLabel();
+//		JLabel [] options = { new JLabel(), new JLabel(), new JLabel() };
+//		options[0].setText("1");
+//		options[1].setText("2");
+//		options[2].setText("3");
 		button.setText("First Button");
 		label.setText("Welcome to BlackJack");
 		parent.add(button);
 		parent.add(label);
+//		parent.add(options[0]);
+//		parent.add(options[1]);
+//		parent.add(options[3]);
 		parent.setVisible(true);
-		parent.setSize(200, 100);
+		parent.setSize(500, 300);
 		parent.setLocationRelativeTo(null);
 	}
 }
