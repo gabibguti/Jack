3ca1d5e0f7fd05b4dd3eb6505211be967b7c8a49
psmeireles <psmeireles25@gmail.com>
2017-11-04 18:20:21 -0200

    Added bank cards

--- 1511241/src/frames/BankFrame.java
+++ 1511241/src/frames/BankFrame.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 
 import components.GameImage;
 import etc.Chip;
+import main.Main;
 import main.Provider;
 
 public class BankFrame extends JFrame {
@@ -26,7 +27,11 @@ public class BankFrame extends JFrame {
 	private JButton bEndGame;
 	private JButton bNewRound;
 	private JButton bSave;
+	private JPanel pComponents;
+	private GridBagConstraints constraints;
 	ArrayList<Card> cards = new ArrayList<>();
+	private ArrayList<JLabel> cardsLabels = new ArrayList<>();
+	Score score = new Score();
 	
 	{
 		chips[0] = new Chip(1);
@@ -59,6 +64,31 @@ public class BankFrame extends JFrame {
 		bNewRound.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
 				BankFrame.newRoundSetEnabled(false);
+				
+				while(BankFrame.bank.cards.isEmpty() == false) {	// Remove all cards from the bank
+					BankFrame.bank.cards.remove(0);
+				}
+				BankFrame.bank.pComponents.remove(0);				
+				
+				score.UpdateScore(cards);
+				while(score.getScore() < 17) {						// Draw cards until score >= 17
+					BankFrame.bank.cards.add(Main.deck.remove(0));
+					score.UpdateScore(BankFrame.bank.cards);
+				}
+				
+				// Insert new cards in the bank
+				int i = 0;
+				BankFrame.bank.constraints.gridy = 0;
+				for(Card c : BankFrame.bank.cards) {
+					BankFrame.bank.constraints.gridx = i+2;								// Insert in the (i+2)th column
+					Icon icon = new ImageIcon(c.getImage());
+					JLabel lb = new JLabel(icon);										// Create Label with image
+					BankFrame.bank.pComponents.add(lb, BankFrame.bank.constraints, i);	// Add to panel
+					i++;
+				}
+				BankFrame.bank.pComponents.revalidate();	// Update frame
+				BankFrame.bank.pComponents.repaint();
+				
 				for(Frame frame: Provider.framesList)
 				{
 					frame.setVisible(true);
@@ -106,24 +136,40 @@ public class BankFrame extends JFrame {
 		// Place buttons under image
 		setLayout(new BorderLayout());
 		
+		// Initial cards
+		cards.add(Main.deck.remove(0));
+		cards.add(Main.deck.remove(0));
+		
 
-		// Draw chip images
-		JPanel pChips = new JPanel();
-		pChips.setLayout(new GridBagLayout());
-		pChips.setOpaque(false);											// Make transparent
-		GridBagConstraints chips_constraints = new GridBagConstraints();	// Defining new constraints
-		chips_constraints.fill = GridBagConstraints.HORIZONTAL;				// Insert images horizontally
-		chips_constraints.gridy = 0;										// Insert in the first row
-		chips_constraints.insets = new Insets(520, 15, 0, 15);				// Padding inside panel layout
-		for(int i = 0; i < chips.length; i++) {
-			chips_constraints.gridx = i;									// Insert in the ith column
+		// Draw components images
+		pComponents = new JPanel();
+		pComponents.setLayout(new GridBagLayout());
+		pComponents.setOpaque(false);										// Make transparent
+		constraints = new GridBagConstraints();								// Defining new constraints
+		constraints.fill = GridBagConstraints.HORIZONTAL;					// Insert images horizontally
+		constraints.gridy = 0;												// Insert in the first row
+		constraints.insets= new Insets(220, 0, 0, 15);						// Padding inside panel layout
+		// Cards
+		int i = 0;
+		for(Card c : cards) {
+			constraints.gridx = i+2;										// Insert in the ith column
+			Icon icon = new ImageIcon(c.getImage());
+			JLabel lb = new JLabel(icon);									// Create Label with image
+			pComponents.add(lb, constraints);								// Add to panel
+			i++;
+		}
+		//Chips
+		constraints.gridy = 1;												// Insert in the second row
+		for(i = 0; i < chips.length; i++) {
+			constraints.gridx = i;											// Insert in the ith column
 			Icon icon = new ImageIcon(chips[i].getImage());
 			JLabel lb = new JLabel(icon);									// Create Label with image
 			lb.addMouseListener(new ChipClickListener(chips[i].getValue()));
-			pChips.add(lb, chips_constraints);								// Add to panel
+			pComponents.add(lb, constraints);								// Add to panel
 		}
 		
-		add(pChips);	// Add chips to bank frame
+		add(pComponents);	// Add chips and cards to bank frame
+		
 		add(pButtons, BorderLayout.PAGE_END);	// Add game buttons to bank frame
 		
         // Allow us to see the frame
