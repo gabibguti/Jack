4bf77c45e5aa8798f23dd74aeaa31cf02b4597f7
gabibguti <gabi_guti_1@hotmail.com>
2017-11-15 01:11:04 -0200

    Extended to paint several images on Panel
    Receives a Map of Images and Points and paints them on Panel
    (Optional Background Image)

--- 1511241/src/components/GameImagePanel.java
+++ 1511241/src/components/GameImagePanel.java
@@ -10,12 +10,15 @@ import javax.swing.JPanel;
 
 public class GameImagePanel extends JPanel{
 	Map<Image, Point> imgs;
-	public GameImagePanel(Map<Image, Point> images) {
+	Image backg;
+	public GameImagePanel(Map<Image, Point> images, Image background) {
 		imgs = images;
+		backg = background;
 	}                                                                                                          	
 	@Override                                                                                                  
     protected void paintComponent(Graphics g) {                                                                
         super.paintComponent(g);
+        g.drawImage(backg, 0, 0, getWidth(), getHeight(), this);
         for (java.util.Map.Entry<Image, Point> e : imgs.entrySet()) {
         	Image img = e.getKey();
         	Point imgPoint = e.getValue();
